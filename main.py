c74cc57d41563cf7cb1d57c3d43105a793e28107
rbrauner <rafalbrauner@gmail.com>
2021-07-03 14:05:39 +0200

    another model

--- main.py
+++ main.py
@@ -8,6 +8,107 @@ from tensorflow.keras.models import Sequential
 
 import pathlib
 
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+from keras import backend as K
+
+# config
+batch_size = 2
+img_height = 1376
+img_width = 1038
+epochs = 2
+
+
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+
+def fit_model(train_ds, val_ds):
+    with tf.device('/gpu:0'):
+        # create model
+        model = Sequential([
+            layers.experimental.preprocessing.Rescaling(
+                1./255, input_shape=(img_height, img_width, 3)),
+            layers.Conv2D(64, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(64, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(128, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(128, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(256, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(256, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(512, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(512, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(1024, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(1024, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Conv2DTranspose(
+                512, (2, 2), strides=(2, 2), padding='same'),
+            layers.Conv2D(512, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(512, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Conv2DTranspose(
+                256, (2, 2), strides=(2, 2), padding='same'),
+            layers.Conv2D(256, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(256, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Conv2DTranspose(
+                128, (2, 2), strides=(2, 2), padding='same'),
+            layers.Conv2D(128, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(128, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same'),
+            layers.Conv2D(64, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Dropout(0.1),
+            layers.Conv2D(64, (3, 3), activation='elu',
+                          kernel_initializer='he_normal', padding='same'),
+            layers.Conv2D(1, (1, 1), activation='sigmoid'),
+        ])
+
+        # compile model
+        model.compile(optimizer='adam',
+                      loss='binary_crossentropy', metrics=[mean_iou])
+
+        # model summary
+        model.summary()
+
+        # train model
+        results = model.fit(train_ds, validation_data=val_ds, batch_size=batch_size, epochs=epochs)
+
+    return results
+
+
 if __name__ == '__main__':
     # prepare data_dir
     data_dir = "data_dir"
@@ -16,11 +117,6 @@ if __name__ == '__main__':
     # get images and images_count
     images = list(data_dir.glob("*/*.*"))
     images_count = len(images)
-
-    # set parameters for the loader
-    batch_size = 2
-    img_height = 1376
-    img_width = 1038
 
     # prepare training data set
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -58,30 +154,32 @@ if __name__ == '__main__':
     first_image = image_batch[0]
     print(np.min(first_image), np.max(first_image))
 
-    # create model
-    num_classes = 5
-    model = Sequential([
-        layers.experimental.preprocessing.Rescaling(
-            1./255, input_shape=(img_height, img_width, 3)),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)
-    ])
+    results = fit_model(train_ds, val_ds)
 
-    # compile model
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
-                      from_logits=True),
-                  metrics=['accuracy'])
+    # # create model
+    # num_classes = 5
+    # model = Sequential([
+    #     layers.experimental.preprocessing.Rescaling(
+    #         1./255, input_shape=(img_height, img_width, 3)),
+    #     layers.Conv2D(16, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Conv2D(32, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Conv2D(64, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Flatten(),
+    #     layers.Dense(128, activation='relu'),
+    #     layers.Dense(num_classes)
+    # ])
 
-    # model summary
-    model.summary()
+    # # compile model
+    # model.compile(optimizer='adam',
+    #               loss=tf.keras.losses.SparseCategoricalCrossentropy(
+    #                   from_logits=True),
+    #               metrics=['accuracy'])
+
+    # # model summary
+    # model.summary()
 
     # # train model
     # epochs = 1
